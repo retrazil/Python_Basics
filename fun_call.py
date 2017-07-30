5f60a188d076d0e5d2244b404033e16b42c2160d
retrazil <retrazil@gmail.com>
2017-07-30 11:12:23 +0530

    basic functions

---
+++ fun_call.py
@@ -0,0 +1,3 @@
+def print_msg(msg, error="no error"):
+    print("log: " + error + "," +  msg)
+print_msg("could not open file", "erra desu")
---
+++ function.py
@@ -0,0 +1,5 @@
+def print_this():
+    print("called print_this")
+
+print("outside of function")
+print_this();

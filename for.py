05fea503c94c41c1f5e797fc004efc8146c3da2c
retrazil <retrazil@gmail.com>
2017-07-30 12:12:14 +0530

    for loop

---
+++ for.py
@@ -0,0 +1,4 @@
+dic = {'boi' : 2, 'bum' :  4}
+
+for i in dic:
+    print(i)
--- if-else.py
+++ if-else.py
@@ -1,5 +1,10 @@
 name = "boi1"
-if name == "boi":
+list = ["boi", "boi1", 3, 5]
+if name in list:
     print("yo " + name)
+
+elif name == "boi1":
+    pass
+
 else:
     print("you are not boi")
